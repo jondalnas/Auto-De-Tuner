f1c840788157cd00591495326db59dba87f69ba5
Jonas Jensen <jdudal11@gmail.com>
2020-02-06 08:20:54 +0100

    Added comments to code

--- DeTuner/DeTuner.ino
+++ DeTuner/DeTuner.ino
@@ -7,10 +7,15 @@
 #include <arduinoFFT.h>
 #include <TimedAction.h>
 
+//Number of samples to use per transformation
 #define SAMPLES 128
+//How many times per second to take a sample
 #define SAMPLING_FREQUENCY 800
+//The input pin
 #define MICROPHONE_IN A0
+//Lowest amplitude to play back
 #define LOW_CUTOFF 25
+//The output pin
 #define SPEAKER 6
 
 arduinoFFT FFT = arduinoFFT();
@@ -24,6 +29,7 @@ double imag[SAMPLES];
 
 float notchVariables[5];
 
+//A class that containes all information about a frequacy to be able to play it back
 class Wave {
   public:
     Wave(uint16_t freq, double amplitude) : freq(freq), amplitude(amplitude) {}
@@ -31,8 +37,10 @@ class Wave {
     double amplitude;
 };
 
+//List of all frequencies the arduino has heard
 std::vector<Wave*> waves = std::vector<Wave*>();
 
+//Generate new notch variables between -6 and 6
 void genNotchVariables() {
   for (int i = 0; i < 5; i++) {
     notchVariables[i] = ((rand() * 2.0 / RAND_MAX) - 1) * 6.0;
@@ -43,11 +51,14 @@ TimedAction updateNotch = TimedAction(1000, genNotchVariables);
 TimedAction sampleTimer = TimedAction(sampleTime, sample);
 
 void setup() {
+  //Begin the serial connection for debugging
   Serial.begin(9600);
 
+  //Set the individual pins to what they need to be
   pinMode(SPEAKER, OUTPUT);
   pinMode(MICROPHONE_IN, INPUT);
 
+  //Reset all notch variables
   for (int i = 0; i < 5; i++) {
     notchVariables[i] = 0;
   }
@@ -55,32 +66,37 @@ void setup() {
 
 int millisec = millis();
 void loop() {
+  //Pause the code untli the next sample needs to be taken
   while(millis() < (millisec + sampleTime)) delay(1);
   millisec = millis();
   sample();
   
-  //sampleTimer.check();
+  //Check if the notches have to be updated
   updateNotch.check();
 }
 
 uint8_t sampleCount = 0;
 void sample() {
+  //Read the input and prepare the variables for a Fourier transformation
   real[sampleCount] = analogRead(MICROPHONE_IN) - 533;
   imag[sampleCount] = 0;
 
   sampleCount++;
 
+  //If the number of samples taken is greater than or equal to the number of samples for Fourier transformation, then continue executing code
   if (sampleCount < SAMPLES) return;
   
   sampleCount = 0;
 
-  /*FFT*/
+  //Perform the Fourier transformation
   FFT.Windowing(real, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
   FFT.Compute(real, imag, SAMPLES, FFT_FORWARD);
   FFT.ComplexToMagnitude(real, imag, SAMPLES);
 
+  //Clear all the old recognized frequencies
   //waves.clear();
 
+  //Loop through all recognized frequencies and add those whos amplitudes is greater than the cutoff to the list of recognized frequencies
   Wave w = Wave(0, 0);
   for (int i = 0; i < (SAMPLES / 2); i++) {
     double amp = real[i];
@@ -95,9 +111,11 @@ void sample() {
     }
   }
   
+  //Play the frequencies back through the speaker
   tone(SPEAKER, w.freq * binToFreq);
 }
 
+//This method is depricated but should be and updated version should be used at a later time
 float getNotch(double freq) {
   //Going one octave higher means doubling the frequency
 
